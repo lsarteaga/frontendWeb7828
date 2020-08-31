101fa3dcdfc02f91a103f9e265f8aed9ad21b8c0
lsarteaga <lsarteaga_dev@protonmail.ch>
2020-08-30 20:02:42 -0500

    uniendo rama

---
+++ src/app/components/contract-projects/contract-projects.component.ts
@@ -0,0 +1,99 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  faEye,
+  faPlus,
+  faPencilAlt,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import swal from 'sweetalert2';
+import {Router} from '@angular/router';
+import {Project} from '../../shared/models/project/project';
+import {ProjectService} from '../../core/services/project/project.service';
+
+@Component({
+  selector: 'app-contract-projects',
+  templateUrl: './contract-projects.component.html',
+  styleUrls: ['./contract-projects.component.css'],
+})
+export class ContractProjectsComponent implements OnInit {
+  @Input() idContract: string;
+  faEye = faEye;
+  faPlus = faPlus;
+  faPencilAlt = faPencilAlt;
+  faTrash = faTrash;
+  projects: Project[];
+  numberPages: number;
+  numberDocs: number;
+  limit = 10;
+  currentPage = 1;
+  pages: Array<number> = [];
+  title: string;
+
+  constructor(private router: Router, private projectService: ProjectService) {
+  }
+
+  ngOnInit(): void {
+    this.count();
+  }
+
+  init(): void {
+    this.title = 'Proyectos Asociados';
+    this.pages = [];
+    this.currentPage = 1;
+  }
+
+  count(): void {
+    this.projectService.count2(this.idContract).subscribe((result) => {
+      console.log(result);
+      this.numberDocs = result.numberDocs;
+      this.calcNumberPages();
+    });
+  }
+
+  calcNumberPages() {
+    this.init();
+    this.numberPages = Math.floor(this.numberDocs / this.limit);
+    this.numberPages++;
+    for (let index = 1; index <= this.numberPages; index++) {
+      this.pages.push(index);
+    }
+    this.loadPage(this.currentPage);
+  }
+
+  changeLimit($event) {
+    this.limit = $event.target.value;
+    this.calcNumberPages();
+  }
+
+  loadPage(pg: number) {
+    this.currentPage = pg;
+    this.projectService
+      .list2(pg, this.limit, this.idContract)
+      .subscribe((result) => {
+        console.log(result);
+        this.projects = result;
+      });
+  }
+
+  delete(project: Project): void {
+    swal
+      .fire({
+        title: '¿Está seguro?',
+        text: `El registro de código ${project.idproject}  será eliminado permanentemente`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar',
+      })
+      .then((option) => {
+        if (option.value) {
+          this.projectService
+            .delete(project.idproject)
+            .subscribe((result) => {
+              this.loadPage(this.currentPage);
+            });
+        }
+      });
+  }
--- src/app/core/services/project/project.service.ts
+++ src/app/core/services/project/project.service.ts
@@ -3,6 +3,8 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { retry } from 'rxjs/operators';
 import { Project } from '../../../shared/models/project/project';
+import { Contract } from '../../../shared/models/contract/contract';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,6 +13,7 @@ export class ProjectService {
   root = 'https://proyecto-javascript-8ecde.web.app/api/';*/
   url = 'http://localhost:5000/api/projects';
   root = 'http://localhost:5000/api/';
+  root2 = 'http://localhost:5000/api/contract/';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -58,6 +61,21 @@ export class ProjectService {
         this.root.concat('page/projects/') + page + '/' + limit,
         this.httpOptions
       )
+      .pipe(retry(1));
+  }
+
+  list2(page: number, limit: number, id: string): Observable<Project[]> {
+    return this.http
+      .get<Project[]>(
+        this.root2.concat('projects/') + id + '/' + page + '/' + limit,
+        this.httpOptions
+      )
+      .pipe(retry(1));
+  }
+
+  count2(id: string): Observable<any> {
+    return this.http
+      .get<any>(this.root2.concat('count/projects/') + id, this.httpOptions)
       .pipe(retry(1));
   }
 }
